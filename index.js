0706873b2988d5faed53ad0950d4752ec338a524
Samson Mubeya <rasspartah@gmail.com>
2024-06-01 23:13:26 +0300

    inititial commits of the board and user getting user inputs

---
+++ index.js
@@ -0,0 +1,24 @@
+let board =[
+    [0,1,0,1,0,1,0,1],
+    [1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1],
+    [1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1],
+    [1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1],
+]
+
+
+let player = "Blue"
+
+function winner(){}
+function capture_opponnent(){}
+function active_player(){}
+
+
+
+
+
+
+console.table(board)
+console.log(`Player ${player}s Turn: Please enter a valid move?` )
